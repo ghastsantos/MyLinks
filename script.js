98c7cda1582551617a52949f787e0826cb423a13
Gastão Santos <gasesb26@gmail.com>
2024-02-12 02:17:12 -0300

    commit .

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//função para realizar o toogleMode(switch) em que caso o html esteja no light mode ao clicar no switch irá mudar para dark (root) e vice-versa
 function toggleMode() {
   const html = document.documentElement
   // if(html.classList.contains('light')){
